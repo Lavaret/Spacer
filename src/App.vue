d6618fa028af7e57617b59b2cbaa7482feaec43f
Lavaret <czarnynozyk@gmail.com>
2019-10-20 16:55:39 +0200

    place for results

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,22 @@ body {
   font-family: "Montserrot", sans-serif;
 }
 
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .6s ease;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+.slide-enter-active, .slide-leave-active {
+  transition: margin-top .4s ease;
+}
+
+.slide-enter, .slide-leave-to {
+  margin-top: -10;
+}
+
 .wrapper {
   margin: 0;
   width: 100%;
@@ -22,16 +38,35 @@ body {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  position: relative;
+
+  &.upper {
+    justify-content: flex-start;
+  }
+
+  .logo {
+    position: absolute;
+    top: 30px;
+    height: 100px;
+  }
 }
 
 </style>
 
 <template>
 
-<div id="app" class="wrapper">
-    <Claim />
-    <SearchInput v-model="searchValue" @input="handleInput" />
-    <HeroImage />
+<div id="app" :class="[{upper: step === 1}, 'wrapper']">
+  <transition name="slide">
+    <img src="./assets/logo.png" class="logo" v-if="step === 1">
+  </transition>
+  <Claim v-if="step === 0"/>
+  <SearchInput v-model="searchValue" @input="handleInput" :dark="step === 1"/>
+  <transition name="fade">
+    <HeroImage v-if="step === 0" />
+  </transition>
+  <div class="results">
+    
+  </div>
 </div>
 
 </template>
@@ -59,13 +94,18 @@ export default {
     return {
       searchValue: '',
       results: [],
+      step: 0,
+      loading: false,
     };
   },
   methods: {
     handleInput: debounce(function () {
+      this.loading = true;
       console.log(this.searchValue);
       axios.get(`${API}?q=${this.searchValue}&media_type=image`)
         .then((response) => {
+          this.loading = false;
+          this.step = 1;
           this.results = response.data.collection.items;
           console.log(this.results);
         })
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,11 +1,12 @@
 <style lang="scss" scoped>
 
 input {
-  height: 30px;
+  height: 35px;
   border: 0;
   margin-top: 50px;
   width: 250px;
   color: white;
+  padding-bottom: 15px;
   text-align: center;
   font-size: 30px;
   font-weight: 300;
@@ -16,14 +17,24 @@ input {
 
 input:focus {
   outline: none;
-  box-shadow: 0 14px 20px -8px rgba(255, 255, 255, .45);
+  box-shadow: 0 14px 20px -12px rgba(255, 255, 255, .45);
+}
+
+.dark {
+  color: #1e3d4a;
+  border-bottom-color: #1e3d4a;
+}
+
+.dark:focus {
+  outline: none;
+  box-shadow: 0 15px 13px -14px rgba(#1e3d4a, .3);
 }
 
 </style>
 
 <template>
 
-<input type="text" id="search" name="search" :value="value" @input="handleChange" />
+<input type="text" id="search" name="search" :class="{ dark }" :value="value" @input="handleChange" />
 
 </template>
 
@@ -36,6 +47,10 @@ export default {
       type: String,
       required: true,
     },
+    dark: {
+      type: Boolean,
+      default: false,
+    }
   },
   methods: {
     handleChange(e) {
