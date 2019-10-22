24d95617b17850914e55a62bab74770c3d8c5a41
Lavaret <czarnynozyk@gmail.com>
2019-10-22 23:12:27 +0200

    loader spinner

--- src/App.vue
+++ src/App.vue
@@ -66,6 +66,67 @@ body {
       grid-template-columns: 1fr 1fr 1fr;
     }
   }
+
+  .lds-ellipsis {
+    display: inline-block;
+    position: relative;
+    width: 64px;
+    height: 64px;
+
+    @media(min-width: 768px) {
+      width: 90px;
+      height: 90px;
+    }
+  }
+  .lds-ellipsis div {
+    position: absolute;
+    top: 127px;
+    width: 11px;
+    height: 11px;
+    border-radius: 50%;
+    background: #333;
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
+  }
+  .lds-ellipsis div:nth-child(1) {
+    left: 6px;
+    animation: lds-ellipsis1 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(2) {
+    left: 6px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(3) {
+    left: 26px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(4) {
+    left: 45px;
+    animation: lds-ellipsis3 0.6s infinite;
+  }
+  @keyframes lds-ellipsis1 {
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+  @keyframes lds-ellipsis3 {
+    0% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(0);
+    }
+  }
+  @keyframes lds-ellipsis2 {
+    0% {
+      transform: translate(0, 0);
+    }
+    100% {
+      transform: translate(19px, 0);
+    }
+  }
 }
 
 </style>
@@ -84,6 +145,7 @@ body {
   <div class="results" v-if="results && !loading && step === 1">
     <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" @click.native="handleModal(item)"/>
   </div>
+  <div class="lds-ellipsis" v-if="step === 1 && loading"><div></div><div></div><div></div><div></div></div>
   <Modal v-if="modal" @closeModal="modal=false" :item="modalItem"/>
 </div>
 
